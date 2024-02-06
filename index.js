aa0d3d85670a6042bafded58c1be9662a42acdcc
joshuadaquila <138364264+joshuadaquila@users.noreply.github.com>
2024-02-06 15:26:57 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 
 const collection = require('./database/index');
 app.use(cors({
-  origin: 'https://navigatorsyouth.netlify.app/',
+  origin: 'https://navigatorsyouth.netlify.app',
 }));
 
 const PORT = 3001;
