363670fbd901aa9115366afe628b793faec5c159
joshuandaquila2 <joshuandaquila24@gmail.com>
2024-02-06 12:04:46 +0800

    new git v1

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+const collection = require('./database/index');
+app.use(cors());
+
+const PORT = 3001;
+app.listen(PORT, (req, res)=>{
+  console.log("Listening on port 3001");
+})
+
+app.get('/feed', async (req,res)=>{
+  try {
+    const feed = await collection.find().toArray();
+    console.log(feed); // Log the result
+    res.send(feed);
+  } catch (error) {
+    console.error(error); // Log any errors
+    res.status(500).send('Internal Server Error');
+  }
+});
